b40158177a77a57359d547e1f84792b1bbf129f3
mhima-sharma <sharmamahima0510@gmail.com>
2026-04-01 16:52:57 +0530

    issuse when we login

--- src/server.js
+++ src/server.js
@@ -25,6 +25,18 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
